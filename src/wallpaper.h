467141644cccffd22421b46f6b411a63dfa6d82b
muhammed251210ai <muhammed251210ai@gmail.com>
2026-03-22 20:43:41 +0300

    Update wallpaper.h

--- src/wallpaper.h
+++ src/wallpaper.h
@@ -1,6 +1,6 @@
-/* * Sovereign Wallpaper Engine - Eternal Glow v230.37
+/* * Sovereign Wallpaper Engine - Animated Win10 Edition v230.38
  * Geliştirici: Muhammed (Kynex)
- * Özellikler: Deep Blue Gradient, Floating Cyber-Stars
+ * Özellikler: Smooth Blue Gradient, Floating Stars
  */
 
 #ifndef WALLPAPER_H
@@ -12,49 +12,40 @@
 #define SOV_BLUE   0x0014
 #define SOV_GLOW   0x5DFF
 
-struct SovereignStar {
+struct Particle {
     float x, y, speed;
-    uint16_t color;
 };
 
-SovereignStar stars[20];
+Particle particles[15];
 
-void initSovereignStars() {
-    for (int i = 0; i < 20; i++) {
-        stars[i].x = random(0, 320);
-        stars[i].y = random(0, 240);
-        stars[i].speed = (float)random(10, 30) / 10.0;
-        stars[i].color = (random(0, 2) == 0) ? 0xFFFF : 0xADFF;
+void initSovereignWallpaper() {
+    for (int i = 0; i < 15; i++) {
+        particles[i].x = random(0, 320);
+        particles[i].y = random(0, 240);
+        particles[i].speed = (float)random(10, 25) / 10.0;
     }
 }
 
 void drawHeroBackground(Adafruit_GFX *tft) {
-    // Muhammed, pürüzsüz geçiş için gradyan çiziyoruz
     for (int i = 0; i < 240; i++) {
-        uint16_t c = (i < 120) ? SOV_DARK : SOV_BLUE;
-        tft->drawFastHLine(0, i, 320, c);
+        tft->drawFastHLine(0, i, 320, (i < 120) ? SOV_DARK : SOV_BLUE);
     }
-    // Windows Logosunun GFX hali
+    // Windows Logosunu Çiz
     tft->fillRect(220, 80, 25, 25, SOV_GLOW);
     tft->fillRect(220, 110, 25, 25, SOV_GLOW);
     tft->fillRect(250, 75, 30, 30, 0xADFF);
     tft->fillRect(250, 110, 30, 30, 0xADFF);
 }
 
-void updateStars(Adafruit_GFX *tft) {
-    for (int i = 0; i < 20; i++) {
-        // Eski yıldızı sil
-        tft->drawPixel((int)stars[i].x, (int)stars[i].y, (stars[i].y < 120) ? SOV_DARK : SOV_BLUE);
-        
-        // Hareket
-        stars[i].x -= stars[i].speed;
-        if (stars[i].x < 0) {
-            stars[i].x = 320;
-            stars[i].y = random(0, 240);
+void updateAnimation(Adafruit_GFX *tft) {
+    for (int i = 0; i < 15; i++) {
+        tft->drawPixel((int)particles[i].x, (int)particles[i].y, (particles[i].y < 120) ? SOV_DARK : SOV_BLUE);
+        particles[i].x -= particles[i].speed;
+        if (particles[i].x < 0) {
+            particles[i].x = 320;
+            particles[i].y = random(0, 240);
         }
-        
-        // Yeni yıldızı çiz
-        tft->drawPixel((int)stars[i].x, (int)stars[i].y, stars[i].color);
+        tft->drawPixel((int)particles[i].x, (int)particles[i].y, 0xFFFF);
     }
 }
 
