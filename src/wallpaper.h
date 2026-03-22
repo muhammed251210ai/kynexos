e4d56dfe973461d003ad5f85a5609193e1ca793b
muhammed251210ai <muhammed251210ai@gmail.com>
2026-03-23 00:44:53 +0300

    Update wallpaper.h

--- src/wallpaper.h
+++ src/wallpaper.h
@@ -1,4 +1,4 @@
-/* * Sovereign Wallpaper Engine - The Omega Core v230.99
+/* * Sovereign Wallpaper Engine - Win10 Absolute v230.100
  * Geliştirici: Muhammed (Kynex)
  */
 
@@ -14,26 +14,27 @@ void drawWin10HeroWallpaper(Adafruit_GFX *tft, bool whiteTheme) {
     uint16_t c_glow = whiteTheme ? 0x03FF : 0x5DFF;
     uint16_t c_logo = whiteTheme ? 0x0000 : 0xFFFF;
 
-    for (int i = 0; i < 240; i++) {
-        uint16_t color = (i < 120) ? c_dark : c_mid;
+    for (int i = 0; i < 215; i++) { // Taskbar sınırına kadar çiz
+        uint16_t color = (i < 100) ? c_dark : c_mid;
         tft->drawFastHLine(0, i, 320, color);
         if (i % 40 == 0) esp_task_wdt_reset();
     }
 
     if (!whiteTheme) {
         for(int i = 0; i < 40; i++) {
-            tft->drawLine(160, 120, 160 + (i*4), 0, c_mid | 0x0808);
-            tft->drawLine(160, 120, 160 - (i*4), 240, c_mid | 0x0808);
+            tft->drawLine(160, 100, 160 + (i*4), 0, c_mid | 0x0808);
+            tft->drawLine(160, 100, 160 - (i*4), 215, c_mid | 0x0808);
         }
     }
 
-    int lx = 145, ly = 95, ls = 36;
+    // Windows 10 Hero Logo
+    int lx = 145, ly = 85, ls = 36;
     int gap = 3, h = ls / 2;
     tft->fillRect(lx, ly, h-gap, h-gap, c_glow);
     tft->fillRect(lx, ly+h, h-gap, h-gap, c_glow);
     tft->fillRect(lx+h, ly-4, h-gap, h+4-gap, c_logo);
     tft->fillRect(lx+h, ly+h, h-gap, h+4-gap, c_logo);
-    tft->drawCircle(160, 115, 45, c_glow);
+    tft->drawCircle(160, 105, 45, c_glow);
     esp_task_wdt_reset();
 }
 
